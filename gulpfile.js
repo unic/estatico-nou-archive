1d0ca39872e9e5c4dfc7ba20c49412556b429ed5
Thomas Jaggi <thomas.jaggi@unic.com>
2017-05-24 11:21:27 +0200

    Loop through tasks to start watchers

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
 const gulp = require('gulp')
-const html = require('estatico-html')
-const validateHtml = require('estatico-html-validate')
-const watch = require('estatico-watch')
 const path = require('path')
 const merge = require('lodash.merge')
 
+const tasks = {
+  html: require('../estatico-html'),
+  validateHtml: require('../estatico-html-validate'),
+  watch: require('../estatico-watch')
+}
+
 // Exemplary custom config
 const config = {
-  html: {
+  html: merge({}, tasks.html.defaults, {
     plugins: {
       // Use JSON file instead of data.js
       data: (file) => {
@@ -18,27 +21,31 @@ const config = {
         }
       }
     }
-  },
-  validateHtml: {
+  }),
+  validateHtml: merge({}, tasks.validateHtml.defaults, {
     src: './build/*.html' // Skip module build, test index only
-  }
+  })
 }
 
 gulp.task('default', gulp.series(
   function htmlTask () {
-    return html.fn(config.html)
+    return tasks.html.fn(config.html)
   },
   function validateHtmlTask () {
-    return validateHtml.fn(config.validateHtml)
+    return tasks.validateHtml.fn(config.validateHtml)
   }
 ))
 
 gulp.task('watch', function watchTask () {
-  watch.fn({
-    task: {
-      fn: html.fn,
-      name: html.name,
-      config: merge({}, html.defaults, config.html)
+  Object.keys(config).forEach((taskName) => {
+    if (config[taskName].watch) {
+      tasks.watch.fn({
+        task: {
+          fn: tasks[taskName].fn,
+          name: tasks[taskName].name,
+          config: config[taskName]
+        }
+      }, gulp)
     }
-  }, gulp)
+  })
 })
