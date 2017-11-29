6fd2cad860ecdcdbb5723df08346370026ece26a
Thomas Jaggi <thomas.jaggi@unic.com>
2017-11-29 09:53:00 +0100

    [Update] Slightly simplified setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,44 @@
-const gulp = require('gulp')
-const path = require('path')
+const gulp = require('gulp');
+const path = require('path');
+const estaticoHandlebars = require('estatico-handlebars')
+const estaticoHtmlValidate = require('estatico-html-validate')
+const estaticoWatch = require('estatico-watch');
 
 // Exemplary custom config
 const config = {
   handlebars: {
     plugins: {
       // Use JSON file instead of data.js
-      data: (file) => {
-        try {
-          return require(file.path.replace(path.extname(file.path), '.json'))
-        } catch (e) {
-          return {}
-        }
-      }
-    }
+      data: file => require(file.path.replace(path.extname(file.path), '.json')), // eslint-disable-line global-require, import/no-dynamic-require
+    },
   },
   validateHtml: {
-    src: './build/*.html' // Skip module build, test index only
+    src: './dist/*.html', // Skip module build, test index only
   },
-  watch: null
-}
+  watch: null,
+};
 
 // Exemplary tasks
 const tasks = {
-  handlebars: require('estatico-handlebars')(config.handlebars),
-  validateHtml: require('estatico-html-validate')(config.validateHtml),
-  watch: require('estatico-watch')(config.watch, gulp)
-}
-
-gulp.task('default', gulp.series(
   // Create named functions so gulp-cli can properly log them
-  function handlebars () {
-    return tasks.handlebars.fn()
-  },
-  function validateHtml () {
-    return tasks.validateHtml.fn()
-  }
-))
+  handlebars: estaticoHandlebars(config.handlebars),
+  validateHtml: estaticoHtmlValidate(config.validateHtml),
+};
 
-gulp.task('watch', function watchTask () {
-  Object.keys(tasks).forEach((taskName) => {
-    if (tasks[taskName].config.watch) {
-      try {
-        tasks.watch.fn({
-          task: tasks[taskName],
-          name: taskName
-        })
-      } catch (err) {
-        // TODO: "Beautify" error handling
-        console.log(err)
-      }
+gulp.task('default', gulp.series(tasks.handlebars.fn, tasks.validateHtml.fn));
+
+gulp.task('watch', () => {
+  Object.keys(tasks).forEach((task) => {
+    const watchTask = estaticoWatch({
+      src: tasks[task].config.watch,
+      fn: tasks[task].fn
+    }, gulp);
+
+    try {
+      watchTask.fn();
+    } catch (err) {
+      // TODO: "Beautify" error handling
+      console.log(err)
     }
-  })
-})
+  });
+});
